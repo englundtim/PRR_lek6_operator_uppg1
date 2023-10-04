759957d53ee6f4b177a2d57910224e0cc9bab44f
Tim <tim.englund@elev.ga.ntig.se>
2023-10-04 10:30:44 +0200

    Lade till storleksräkning med tal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("Uppgift a)");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Skriv in tal 1");
+        int tal1 = in.nextInt();
+        System.out.println("Skriv in tal 2");
+        int tal2 = in.nextInt();
+
+        System.out.println("Är tal1 = tal2?");
+        System.out.println(tal1==tal2);
+        System.out.println("Är tal1 < tal2?");
+        System.out.println(tal1<=tal2);
+        System.out.println("Är tal1 > tal2?");
+        System.out.println(tal1>=tal2);
+
     }
 }
