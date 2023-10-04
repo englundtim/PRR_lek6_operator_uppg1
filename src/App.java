85e9029c7452a7ac40c4e77ee3b2a2688710229c
Tim <tim.englund@elev.ga.ntig.se>
2023-10-04 10:53:57 +0200

    Lade till konstig talsmagi

--- src/App.java
+++ src/App.java
@@ -28,8 +28,17 @@ public class App {
         System.out.println("Är 4 = 5 eller 2?");
         System.out.println(fält1[0]==fält1[1]);
         System.out.println(fält1[0]==fält1[2]);
-        
 
-
+        System.out.println("Uppgift c)");
+        int tal = in.nextInt();
+        int talStart=tal;
+        tal+=7;
+        tal*=2;
+        tal-=6;
+        tal/=2;
+        tal+=3;
+        tal-=talStart;
+        System.out.println("Blev talet 7?");
+        System.out.println(tal==7);
     }
 }
